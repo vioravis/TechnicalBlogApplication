1df1f8aea8c4a80363c5d8a9e202f3f9aed4d457
vioravis <vioravis@gmail.com>
2018-11-24 14:29:02 +0530

    First Milestone

---
+++ src/main/java/techblog/TechnicalBlogApplication.java
@@ -0,0 +1,14 @@
+package techblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalBlogApplication {
+    public static void main(String [] args) {
+        SpringApplication.run(TechnicalBlogApplication.class,args);
+    }
+}
+
+
+
